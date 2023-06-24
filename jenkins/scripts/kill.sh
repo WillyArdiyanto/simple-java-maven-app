c3cf82607f42a3eefc7e39d2ffc4b13b1eb69d6a
WillyArdiyanto <willyardiyanto2@gmail.com>
2023-06-24 11:39:57 +0700

    Update: kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,11 +1,3 @@
-# #!/usr/bin/env sh
-
-# echo 'The following command terminates the "npm start" process using its PID'
-# echo '(written to ".pidfile"), all of which were conducted when "deliver.sh"'
-# echo 'was executed.'
-# set -x
-# kill $(cat .pidfile)
-
 #!/usr/bin/env bash
 
 # Find the process ID (PID) of the running Java application
